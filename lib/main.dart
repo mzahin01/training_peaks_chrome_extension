6a6a001957787ae3516906c1e5bfb0c1b83631e5
mahidul-islam <mizihan84@gmail.com>
2024-01-24 15:12:04 +0600

    training peaks color update working

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,47 @@
+// ignore_for_file: avoid_web_libraries_in_flutter
+
 import 'package:flutter/material.dart';
+// import 'package:web/web.dart' as w;
+import 'dart:js' as js;
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String? output;
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World 123'),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Text('Hello World 123'),
+                const SizedBox(
+                  height: 20,
+                ),
+                MaterialButton(
+                  onPressed: () {
+                    js.context.callMethod('changeColor', ['#3aa757']);
+                  },
+                  child: const Text('Print to Console'),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(output ?? 'NO output'),
+              ],
+            ),
+          ),
         ),
       ),
     );
